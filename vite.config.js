7b5ebedac71986c014a8ee3bcd1d76b7afe57cc5
Abdullah Çanakçı <abdullahcanakci.0@gmail.com>
2021-09-12 16:21:04 +0300

    package build

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,21 @@
 import { defineConfig } from 'vite';
 import reactRefresh from '@vitejs/plugin-react-refresh';
+const path = require('path');
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [reactRefresh()],
+  build: {
+    lib: !process.env.BUILD_APP ? {
+      entry: path.resolve(__dirname, 'lib/main.js'),
+      name: 'DatePicker',
+      fileName: format => `date-picker.${format}.js`
+    } : undefined,
+    
+    rollupOptions: {
+      external: ['react', 'react-dom']
+    }
+  },
   css: {
     modules: {
       localsConvention: 'camelCaseOnly'      
